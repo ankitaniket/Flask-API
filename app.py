a719c115cb0405fa0299ff4f72b01b3f8c93227e
ankitaniket <ankitaniket3@gmail.com>
2024-01-20 21:35:56 +0530

    Added CORS policy

--- app.py
+++ app.py
@@ -1,31 +1,61 @@
-from PIL import Image
 import tensorflow as tf
-from flask import Flask, request
+from flask import Flask, request,jsonify
+from keras.preprocessing.image import ImageDataGenerator
 from PIL import Image
 import numpy as np
+from io import BytesIO
+import requests
 from keras.applications.inception_v3 import InceptionV3, preprocess_input
-from flask import Flask, request
+from flask import Flask, request, jsonify, make_response
 from flask_cors import CORS
+
 
 app = Flask(__name__)
 CORS(app)
 
-model = tf.keras.models.load_model("best_model.h5")
+model = tf.keras.models.load_model("Final best_model-2.h5")
 
-# platform Darwin-22.4.0-x86_64-i386-64bit
-
-
-
+@app.errorhandler(Exception)
+def handle_error(e):
+    app.logger.error(str(e))
+    response = jsonify({'error': str(e)})
+    response.status_code = 500
+    return response
 
 @app.route('/', methods=['POST'])
 def predict():
-    # Get the uploaded image file
-    image_file = request.files['image']
+    if request.headers['Content-Type'] == 'application/json':
+        # If the request is in JSON format
+        data = request.get_json()
+        image_url = data.get('image_url', '')
+    else:
 
-    # Load the model
+        # If the request is in form data format
 
-    # Predict & classify image
-    img = Image.open(image_file).convert('RGB')
+        image_file = request.files.get('image', None)
+        if image_file:
+            img = Image.open(image_file).convert('RGB')
+            img = img.resize((256, 256))
+            img_array = np.array(img)
+            img_array = preprocess_input(img_array)
+            input_arr = np.array([img_array])
+            pred = np.argmax(model.predict(input_arr))
+
+            if pred == 0:
+                result = "This image does not contain a pothole."
+            else:
+                result = "This image contains a pothole."
+
+            response = make_response(jsonify({'result': result}), 200)
+            print("This is result - ",result)
+            response.headers['Access-Control-Allow-Origin'] = '*'
+            return response
+        else:
+            return jsonify({'error': 'Image file not provided'}), 400
+
+    response = requests.get(image_url)
+    img = Image.open(BytesIO(response.content)).convert('RGB')
+
     img = img.resize((256, 256))
     img_array = np.array(img)
     img_array = preprocess_input(img_array)
@@ -37,14 +67,8 @@ def predict():
     else:
         result = "This image contains a pothole."
 
-    # Add the Access-Control-Allow-Origin header to the response
-    # response = jsonify({'result': result})
-    # response.headers.add('Access-Control-Allow-Origin', '*')
-    return result
-
-    # Return the prediction result as a JSON object
+    response = make_response(jsonify({'result': result}), 200)
     return response
 
-
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, port=8001)
