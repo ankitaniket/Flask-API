2e72833b2cc6646e3233d5ac54e32e8fcd8e2f66
ankitaniket <ankitaniket3@gmail.com>
2024-01-27 20:53:30 +0530

    Add test and index routes

--- app.py
+++ app.py
@@ -1,20 +1,22 @@
 import tensorflow as tf
-from flask import Flask, request,jsonify
-from keras.preprocessing.image import ImageDataGenerator
+from flask import Flask, request,jsonify,render_template,make_response,render_template_string
 from PIL import Image
 import numpy as np
 from io import BytesIO
 import requests
-from keras.applications.inception_v3 import InceptionV3, preprocess_input
-from flask import Flask, request, jsonify, make_response
+from keras.applications.inception_v3 import preprocess_input
 from flask_cors import CORS
+import uuid
 
 
 app = Flask(__name__)
 CORS(app)
 
+# Python 3.9.18
+# env conda
 model = tf.keras.models.load_model("Final best_model-2.h5")
 
+# Your Firebase configuration
 @app.errorhandler(Exception)
 def handle_error(e):
     app.logger.error(str(e))
@@ -22,16 +24,18 @@ def handle_error(e):
     response.status_code = 500
     return response
 
+
+def generate_unique_filename(result):
+    prefix = 'Y' if result == "This image contains a pothole." else ''
+    return f"{prefix}{uuid.uuid4()}"  
+
 @app.route('/', methods=['POST'])
 def predict():
     if request.headers['Content-Type'] == 'application/json':
-        # If the request is in JSON format
+
         data = request.get_json()
         image_url = data.get('image_url', '')
     else:
-
-        # If the request is in form data format
-
         image_file = request.files.get('image', None)
         if image_file:
             img = Image.open(image_file).convert('RGB')
@@ -47,7 +51,9 @@ def predict():
                 result = "This image contains a pothole."
 
             response = make_response(jsonify({'result': result}), 200)
-            print("This is result - ",result)
+            print("This is result - ", result)
+
+
             response.headers['Access-Control-Allow-Origin'] = '*'
             return response
         else:
@@ -68,7 +74,55 @@ def predict():
         result = "This image contains a pothole."
 
     response = make_response(jsonify({'result': result}), 200)
+
     return response
 
+@app.route('/whoami')
+def whoami():
+    # Fetch the IP address using ipify.org
+    ipify_response = requests.get("https://api.ipify.org/?format=json")
+    ipify_data = ipify_response.json()
+    
+    geo_response = requests.get(f"http://www.geoplugin.net/json.gp?ip={ipify_data['ip']}")
+    geo_data = geo_response.json()
+    
+    # Format the output with styling for the terminal
+    output = """
+    \033[1mIP Address:\033[0m {ip}
+    \033[1mCity:\033[0m {city}
+    \033[1mRegion:\033[0m {region} (\033[36m{region_code}\033[0m)
+    \033[1mCountry:\033[0m {country} (\033[36m{country_code}\033[0m)
+    \033[1mContinent:\033[0m {continent} (\033[36m{continent_code}\033[0m)
+    \033[1mLatitude:\033[0m {latitude}
+    \033[1mLongitude:\033[0m {longitude}
+    \033[1mTimezone:\033[0m {timezone}
+    \033[1mCurrency:\033[0m {currency} (\033[36m{currency_symbol}\033[0m)
+    """.format(
+        ip=geo_data['geoplugin_request'],
+        city=geo_data['geoplugin_city'],
+        region=geo_data['geoplugin_region'],
+        region_code=geo_data['geoplugin_regionCode'],
+        country=geo_data['geoplugin_countryName'],
+        country_code=geo_data['geoplugin_countryCode'],
+        continent=geo_data['geoplugin_continentName'],
+        continent_code=geo_data['geoplugin_continentCode'],
+        latitude=geo_data['geoplugin_latitude'],
+        longitude=geo_data['geoplugin_longitude'],
+        timezone=geo_data['geoplugin_timezone'],
+        currency=geo_data['geoplugin_currencyCode'],
+        currency_symbol=geo_data['geoplugin_currencySymbol']
+    )
+    
+    return output
+
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/test', methods=['GET'])
+def test():
+    return render_template_string('Yepp, your curl command is running successfully for the test route. 😄')
+
 if __name__ == '__main__':
-    app.run(debug=True, port=8001)
+    app.run(debug=True, port=8080)
